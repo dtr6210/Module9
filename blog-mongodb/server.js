8f4e65e7882b216870cc8ba2c2bf55cfec47f2cd
dtr6210 <rialsdean@gmail.com>
2024-02-02 11:52:48 -0600

    completed exercise 1

---
+++ blog-mongodb/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+// parse requests of content-type -application/json
+
+let dbConnect = require("./dbConnect")
+let userRoutes = require('./routes/userRoutes')
+
+app.use(express.json());
+app.use('/api/users', userRoutes)
+
+
+app.get("/", (req, res) => {
+ res.json({ message: "Welcome to myMongoDB application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+ console.log(`Server is running on port
+${PORT}.`);
+});
